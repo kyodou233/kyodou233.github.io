5ce2dd18628948d7c0f10cab3916879933c677d0
kyodou233 <kyodou2026@gmail.com>
2026-04-11 15:09:57 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+//import autoprefixer from 'autoprefixer'
 
 export default defineUserConfig({
   base: '/',
