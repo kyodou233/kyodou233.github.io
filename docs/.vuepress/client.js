d342b3ac0aff85d36a48da0a2f31866834888adc
kyodou233 <kyodou2026@gmail.com>
2026-04-27 21:06:27 +0800

    Add files via upload

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,4 +1,6 @@
-import { defineClientConfig } from 'vuepress/client'
+import { defineClientConfig } from "vuepress/client";
+import { h } from "vue";
+import { Layout } from "vuepress-theme-plume/client";
 // import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
 // import NpmBadge from 'vuepress-theme-plume/features/NpmBadge.vue'
 // import NpmBadgeGroup from 'vuepress-theme-plume/features/NpmBadgeGroup.vue'
@@ -6,34 +8,41 @@ import { defineClientConfig } from 'vuepress/client'
 
 // import CustomComponent from './theme/components/Custom.vue'
 
-import './theme/styles/custom.css'
+import "./theme/styles/custom.css";
 
-import MyProfile from './theme/components/MyProfile.vue'
-import MyType from './theme/components/MyType.vue'
-import MyHomeBg from './theme/components/MyHomeBg.vue'
-import SiteInfo from './theme/components/SiteInfo.vue'
-import VisitingCounter from './theme/components/VisitingCounter.vue'
-import Typed from 'typed.js';
-import { siteInfoPlugin } from './theme/plugins/site-info.js'
-import FontLoader from './theme/components/FontLoader.vue'
+import MyProfile from "./theme/components/MyProfile.vue";
+import MyType from "./theme/components/MyType.vue";
+import MyHomeBg from "./theme/components/MyHomeBg.vue";
+import SiteInfo from "./theme/components/SiteInfo.vue";
+import VisitingCounter from "./theme/components/VisitingCounter.vue";
+import Typed from "typed.js";
+import { siteInfoPlugin } from "./theme/plugins/site-info.js";
+import FontLoader from "./theme/components/FontLoader.vue";
 
 export default defineClientConfig({
-  enhance({ app }) {
-    // built-in components
-    // app.component('RepoCard', RepoCard)
-    // app.component('NpmBadge', NpmBadge)
-    // app.component('NpmBadgeGroup', NpmBadgeGroup)
-    // app.component('Swiper', Swiper) // you should install `swiper`
+    layouts: {
+        Layout: () =>
+            h(Layout, null, {
+                "layout-top": () => h(FontLoader),
+            }),
+    },
 
-    // your custom components
-    // app.component('CustomComponent', CustomComponent)
-    app.component('MyProfile', MyProfile)
-    app.component('MyType', MyType)
-    app.component('SiteInfo', SiteInfo)
-    app.component('MyHomeBg', MyHomeBg)
-    app.component('VisitingCounter', VisitingCounter)
-    app.component('FontLoader', FontLoader)
-    app.directive('typed', Typed)
-    app.use(siteInfoPlugin)
-  },
-})
+    enhance({ app }) {
+        // built-in components
+        // app.component('RepoCard', RepoCard)
+        // app.component('NpmBadge', NpmBadge)
+        // app.component('NpmBadgeGroup', NpmBadgeGroup)
+        // app.component('Swiper', Swiper) // you should install `swiper`
+
+        // your custom components
+        // app.component('CustomComponent', CustomComponent)
+        app.component("MyProfile", MyProfile);
+        app.component("MyType", MyType);
+        app.component("SiteInfo", SiteInfo);
+        app.component("MyHomeBg", MyHomeBg);
+        app.component("VisitingCounter", VisitingCounter);
+        app.component("FontLoader", FontLoader);
+        app.directive("typed", Typed);
+        app.use(siteInfoPlugin);
+    },
+});
